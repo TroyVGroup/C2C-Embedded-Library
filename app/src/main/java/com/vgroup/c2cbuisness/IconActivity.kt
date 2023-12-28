142ab830fdb00b823feb83a9ecea74049601481f
Naresh <nareshr@vgroupinc.com>
2023-12-28 16:10:19 +0530

    code refactoring

--- app/src/main/java/com/vgroup/c2cbuisness/IconActivity.kt
+++ app/src/main/java/com/vgroup/c2cbuisness/IconActivity.kt
@@ -1,15 +1,16 @@
 package com.vgroup.c2cbuisness
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.vgroup.c2cembedcode.C2CVoiceActivity
-import com.vgroup.c2cembedcode.CallActivity
 import com.vgroup.c2cembedcode.C2CConstants
+import com.vgroup.c2cembedcode.C2CVoiceActivity
 import com.vgroup.c2cembedcode.pojo.Modes
 
 class IconActivity : AppCompatActivity() {
@@ -19,20 +20,23 @@ class IconActivity : AppCompatActivity() {
     var email_icon: ImageView? = null
     var modes: Modes = Modes()
     val ALL_PERMISSIONS = 101
-    val channelId = "64538a3e23426f1086e09e9b"
-    val origin = "https://testvg20.weebly.com"
-    val website = "https://testvg20.weebly.com/naresh-new-page.html"
-    var c2cVoiceActivity: C2CVoiceActivity = C2CVoiceActivity(this@IconActivity,origin,website)
+    val channelId = "657ad6d2632f4ca91909cdbc"
+//    val origin = "https://testvg20.weebly.com"
+//    val website = "https://testvg20.weebly.com/naresh-new-page.html"
+//    val packageName = "com.example.android"
+    var c2cVoiceActivity: C2CVoiceActivity = C2CVoiceActivity(this@IconActivity,"com.example.android","com.example.android")
+    lateinit var locationManager: LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.icon_activity)
 
-        call_icon = findViewById(R.id.call_icon)
-        msg_icon = findViewById(R.id.msg_icon)
-        email_icon = findViewById(R.id.email_icon)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        call_icon = findViewById(R.id.c2cCall)
+        msg_icon = findViewById(R.id.c2cMsg)
+        email_icon = findViewById(R.id.c2cEmail)
 
-        c2cVoiceActivity.getModes(channelId, origin,website, modes, call_icon, msg_icon, email_icon)
+        c2cVoiceActivity.getModes(channelId, "com.example.android","com.example.android", modes, call_icon, msg_icon, email_icon)
 
         call_icon!!.setOnClickListener {
             val permission = ContextCompat.checkSelfPermission(
@@ -40,22 +44,23 @@ class IconActivity : AppCompatActivity() {
                 android.Manifest.permission.RECORD_AUDIO
             )
             if (permission == PackageManager.PERMISSION_GRANTED) {
-                c2cVoiceActivity.getCallDetails(channelId, modes, C2CConstants.CALL)
+                c2cVoiceActivity.getCallDetails(channelId, modes, C2CConstants.CALL, locationManager)
             } else {
                 c2cVoiceActivity.showError("Message", "Allow permission from setting to make call")
             }
         }
 
         msg_icon!!.setOnClickListener {
-            c2cVoiceActivity.getCallDetails(channelId, modes, C2CConstants.SMS)
+            c2cVoiceActivity.getCallDetails(channelId, modes, C2CConstants.SMS,locationManager)
         }
         email_icon!!.setOnClickListener {
-            c2cVoiceActivity.getCallDetails(channelId, modes, C2CConstants.EMAIL)
+            c2cVoiceActivity.getCallDetails(channelId, modes, C2CConstants.EMAIL,locationManager)
         }
 
         val permissions = arrayOf(
             Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.ACCESS_FINE_LOCATION
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
         )
 
         ActivityCompat.requestPermissions(this, permissions, ALL_PERMISSIONS)
